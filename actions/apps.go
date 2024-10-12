6965ab072e7990bb47d0b06453d845a10e78c555
Shubham Singh Chahar <thefallenmerc@gmail.com>
2024-10-12 23:55:45 +0530

    added some more helper functions

--- actions/apps.go
+++ actions/apps.go
@@ -4,6 +4,17 @@ import (
 	"fmt"
 	"os/exec"
 	"strings"
+	"syscall"
+	"unsafe"
+
+	"golang.org/x/sys/windows"
+)
+
+// Declare types for Windows API functions
+var (
+	procEnumWindows     = user32.NewProc("EnumWindows")
+	procGetWindowTextW  = user32.NewProc("GetWindowTextW")
+	procIsWindowVisible = user32.NewProc("IsWindowVisible")
 )
 
 func LaunchWebAddress(webAddress *string) {
@@ -33,6 +44,70 @@ func DetectIfAppRunning(appPath *string, appArguments *string) bool {
 	}
 
 	return false
+}
+
+func DetectIfAppRunningByProcessName(processName string) bool {
+
+	snapshot, err := windows.CreateToolhelp32Snapshot(windows.TH32CS_SNAPPROCESS, 0)
+	if err != nil {
+		fmt.Printf("Failed to take process snapshot: %v\n", err)
+		return false
+	}
+	defer windows.CloseHandle(snapshot)
+
+	var entry windows.ProcessEntry32
+	entry.Size = uint32(unsafe.Sizeof(entry))
+
+	if err = windows.Process32First(snapshot, &entry); err != nil {
+		fmt.Printf("Failed to retrieve first process: %v\n", err)
+		return false
+	}
+
+	for {
+		process := windows.UTF16ToString(entry.ExeFile[:])
+		if process == processName {
+			return true
+		}
+		if err = windows.Process32Next(snapshot, &entry); err != nil {
+			break
+		}
+	}
+
+	return false
+}
+func DetectIfAppRunningByTitle(appTitle string) bool {
+	var found bool
+	enumWindows := func(hwnd syscall.Handle, lParam uintptr) uintptr {
+		title := getWindowTitle(hwnd)
+		if len(title) > 0 && isWindowVisible(hwnd) && contains(appTitle, title) {
+			found = true
+			return 0 // Stop enumeration
+		}
+		return 1 // Continue enumeration
+	}
+
+	// Enumerate all windows
+	procEnumWindows.Call(syscall.NewCallback(enumWindows), 0)
+
+	return found
+}
+
+// getWindowTitle retrieves the window title for a given window handle.
+func getWindowTitle(hwnd syscall.Handle) string {
+	buf := make([]uint16, 256)
+	procGetWindowTextW.Call(uintptr(hwnd), uintptr(unsafe.Pointer(&buf[0])), uintptr(len(buf)))
+	return syscall.UTF16ToString(buf)
+}
+
+// isWindowVisible checks if a window is visible.
+func isWindowVisible(hwnd syscall.Handle) bool {
+	ret, _, _ := procIsWindowVisible.Call(uintptr(hwnd))
+	return ret != 0
+}
+
+// contains checks if the window title contains "Discord".
+func contains(appTitle string, title string) bool {
+	return strings.Contains(title, appTitle)
 }
 
 // FocusDiscord brings the Discord window to focus.
--- go.mod
+++ go.mod
@@ -4,11 +4,15 @@ go 1.22
 
 toolchain go1.22.1
 
-require github.com/wailsapp/wails/v2 v2.9.2
+require (
+	github.com/go-ole/go-ole v1.3.0
+	github.com/moutend/go-wca v0.3.0
+	github.com/wailsapp/wails/v2 v2.9.2
+	golang.org/x/sys v0.25.0
+)
 
 require (
 	github.com/bep/debounce v1.2.1 // indirect
-	github.com/go-ole/go-ole v1.3.0 // indirect
 	github.com/godbus/dbus/v5 v5.1.0 // indirect
 	github.com/google/uuid v1.3.0 // indirect
 	github.com/jchv/go-winloader v0.0.0-20210711035445-715c2860da7e // indirect
@@ -33,7 +37,6 @@ require (
 	golang.org/x/crypto v0.23.0 // indirect
 	golang.org/x/exp v0.0.0-20240904232852-e7e105dedf7e // indirect
 	golang.org/x/net v0.25.0 // indirect
-	golang.org/x/sys v0.25.0 // indirect
 	golang.org/x/text v0.18.0 // indirect
 )
 
